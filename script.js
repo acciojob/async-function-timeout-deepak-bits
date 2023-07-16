f2432da10732c0797f7fc534cf2b6f6775cc9a64
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-16 22:40:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+const showMessage = async () => {
+    const text = document.getElementById('text').value;
+    const delay = +document.getElementById('delay').value;
+
+    if(!text || !delay) return;
+
+    await new Promise((resolve) => setTimeout(resolve, delay));
+
+    document.getElementById('output').textContent = text;
+}
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', showMessage);
+
